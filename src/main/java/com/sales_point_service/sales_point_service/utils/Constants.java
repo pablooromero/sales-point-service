ffa3546057a92f6e8a02c37588813d8dddafb99e
pablooromero <pabloromerook@gmail.com>
2025-04-27 22:48:40 -0300

    added constants file

---
+++ src/main/java/com/sales_point_service/sales_point_service/utils/Constants.java
@@ -0,0 +1,39 @@
+package com.sales_point_service.sales_point_service.utils;
+
+public class Constants {
+    public static final String COST_NOT_NEGATIVE="Cost cannot be negative";
+    public static final String COST_NOT_EXISTS="Cost does not exist";
+    public static final String COST_DELETED="Cost removed successfully";
+    public static final String NOT_DIRECT_CONNECTIONS="No direct connections found for this SalePoint";
+    public static final String SALE_POINTS_NOT_CONNECTED="One or both SalePoints are not connected";
+    public static final String SALE_POINTS_NOT_FOUND="Sale Point not found with ID: ";
+    public static final String SALE_POINTS_DELETED="Sale Point removed successfully with ID: ";
+    public static final String SALE_POINTS_NAME_NOT_NULL="Name cannot be null";
+
+    public static final String SAVING_COST="Saving cost: ";
+    public static final String COST_SAVED_SUCCESSFULLY="Cost saved successfully";
+    public static final String GET_ALL_COSTS="Retrieving all costs";
+    public static final String GET_ALL_COSTS_SUCCESSFULLY="Retrieved all costs successfully";
+    public static final String CREATING_COST="Creating cost";
+    public static final String COST_CREATED_SUCCESSFULLY="Cost created successfully";
+    public static final String DELETING_COST="Deleting cost: ";
+    public static final String COST_DELETED_SUCCESSFULLY="Cost deleted successfully";
+    public static final String GET_DIRECT_CONNECTIONS="Retrieving direct connections";
+    public static final String GET_DIRECT_CONNECTIONS_SUCCESSFULLY="Retrieved all direct connections successfully";
+    public static final String GET_SHORTEST_PATH="Retrieving shortest path";
+    public static final String GET_SHORTEST_PATH_SUCCESSFULLY="Retrieved shortest path successfully";
+
+
+    public static final String SAVING_SALE_POINT="Saving sale point: ";
+    public static final String SALE_POINT_SAVED_SUCCESSFULLY="Sale point saved successfully";
+    public static final String GET_ALL_SALES_POINT="Retrieving all sales points";
+    public static final String GET_ALL_SALES_POINT_SUCCESSFULLY="Retrieved all sales points successfully";
+    public static final String GET_SALE_POINT="Retrieving sale point with ID: ";
+    public static final String GET_SALE_POINT_SUCCESSFULLY="Retrieved sale point successfully";
+    public static final String CREATING_SALE_POINT="Creating sale point: ";
+    public static final String SALE_POINT_CREATED_SUCCESSFULLY="Sale point created successfully";
+    public static final String UPDATING_SALE_POINT="Updating sale point with ID: ";
+    public static final String SALE_POINT_UPDATED_SUCCESSFULLY="Sale point updated successfully";
+    public static final String DELETING_SALE_POINT="Deleting sale point with ID: ";
+    public static final String SALE_POINT_DELETED_SUCCESSFULLY="Sale point deleted successfully";
+}
